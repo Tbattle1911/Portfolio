ee5a0121401097c4fe6c27d137e0c52741e90110
Tbattle1911 <mr.battle1911@gmail.com>
2022-06-29 05:13:13 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// typed.js //
+
+var typed = new Typed(".name", {
+    strings: [" Thurman Battle.","a Web Developer.", "a Photographer.","a UX Designer"],
+    typeSpeed: 50,
+    backSpeed: 40,
+    loop: true,
+});
